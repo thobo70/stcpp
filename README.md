f843d2d7f67201459bbc5a0275ad7acac7e95628
Thomas Boos <tboos70@gmail.com>
2024-08-16 10:04:24 +0000

    update test and readme

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ stcpp (super tiny cpp) is a simple and resource efficient C Preprocessor designe
 - File inclusion
 - Conditional compilation
 
+## Shortcommings
+
+- no shortcuts for && and ||, may lead to errors in complex header files
+- precedence of operators needs a fix
+- several other @todo's
+- error messages are not yet polished
+- no #line used for next compile stages
+
 ## Installation
 
 Clone the repository and navigate to the project directory:
@@ -15,8 +23,11 @@ Clone the repository and navigate to the project directory:
 ```sh
 git clone https://github.com/thobo70/stcpp.git
 cd stcpp
+make
+make test
 ```
 
 ## Status
 
-still in a early development state, not yet usable for compiling a source code
+it is getting usable, but has still trouble with very complex header files 
+
--- test/test.c
+++ test/test.c
@@ -13,6 +13,9 @@
 #define LOCAL_MACRO 200
 #define COMPLEX_MACRO (HEADER_MACRO + LOCAL_MACRO)
 
+#define TEST_MACRO(x) ((x) * (x))
+#define CONDITIONAL_MACRO(x, y) ((x) > (y) ? (x) : (y))
+
 void headerFunction() {
   // Custom implementation for testing
 }
@@ -58,6 +61,42 @@ int main() {
   #else
   right(4);
   #endif
+
+#if defined(HEADER_MACRO)
+  #if HEADER_MACRO > 100
+    #if HEADER_MACRO < 300
+      #if HEADER_MACRO == 150
+        // Code for HEADER_MACRO == 150
+        wrong(5);
+      #else
+        // Code for HEADER_MACRO between 100 and 300 but not 150
+        wrong(6);
+      #endif
+    #else
+      // Code for HEADER_MACRO >= 300
+      wrong(7);
+    #endif
+  #else
+    // Code for HEADER_MACRO <= 100
+    right(8);
+  #endif
+#else
+  // Code for HEADER_MACRO not defined
+  wrong(8);
+#endif
+
+#IF TEST_MACRO(5) != 25
+  #error "TEST_MACRO(5) != 25"
+#ELSE
+  right(9);
+#ENDIF
+
+#IF CONDITIONAL_MACRO(5, 10) != 10
+  #error "CONDITIONAL_MACRO(5, 10) != 10"
+#ELSE
+  right(10);
+#ENDIF
+
 
   return 0;
 }
